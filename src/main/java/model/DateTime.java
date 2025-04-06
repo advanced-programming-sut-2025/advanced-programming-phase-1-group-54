98069a1939e459445ab7bf4c7a20988eb48aa132
Yasin HajiKhalili <seidyasinhajikhalili@gmail.com>
2025-04-06 15:07:32 -0700

    update class datetime

--- src/main/java/model/DateTime.java
+++ src/main/java/model/DateTime.java
@@ -1,10 +1,13 @@
 package model;
 
+import model.enums.WeekDay;
+
 public class DateTime {
     private int year;
     private int month;
     private int day;
     private int hour;
+    private WeekDay weekDay;
 
     public int getYear() {
         return year;
@@ -22,7 +25,12 @@ public class DateTime {
         return hour;
     }
 
+    public WeekDay getWeekDay() {
+        return weekDay;
+    }
+
     public void increaseMonth(int amount) {
+        weekDay = WeekDay.values()[(weekDay.ordinal() + amount * 30)%7];
         year += (amount + month)/12;
         month = (amount + month)%12;
         if(month == 0){
@@ -31,6 +39,7 @@ public class DateTime {
     }
 
     public void increaseDay(int amount) {
+        weekDay = WeekDay.values()[(amount + day + weekDay.ordinal())%7];
         increaseMonth((amount + day)/30);
         day = (amount + day)%30;
         if(day == 0){
