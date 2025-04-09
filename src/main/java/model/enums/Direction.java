3f6a9b45a4a7da46d6d9d213d742dbaa662c0223
Kourosh Kaveh <kourosh.kaveh84@gmail.com>
2025-04-09 11:38:06 +0330

    improved location and direction

--- src/main/java/model/enums/Direction.java
+++ src/main/java/model/enums/Direction.java
@@ -1,5 +1,28 @@
 package model.enums;
 
 public enum Direction {
-    UP, DOWN, LEFT, RIGHT,UP_LEFT,DOWN_LEFT,UP_RIGHT,DOWN_RIGHT;
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0,1),
+    UP_LEFT(-1, -1),
+    DOWN_LEFT(1, -1),
+    UP_RIGHT(-1, 1),
+    DOWN_RIGHT(1, 1);
+
+    private final int dRow;
+    private final int dColumn;
+
+    Direction(int dRow, int dColumn) {
+        this.dRow = dRow;
+        this.dColumn = dColumn;
+    }
+
+    public int dRow() {
+        return dRow;
+    }
+
+    public int dColumn() {
+        return dColumn;
+    }
 }
--- src/main/java/model/map/Location.java
+++ src/main/java/model/map/Location.java
@@ -1,4 +1,9 @@
 package model.map;
 
-public record Location(int x, int y) {
+import model.enums.Direction;
+
+public record Location(int row, int column) {
+    public Location getLocationAt(Direction direction) {
+        return new Location(row + direction.dRow(), column + direction.dColumn());
+    }
 }
