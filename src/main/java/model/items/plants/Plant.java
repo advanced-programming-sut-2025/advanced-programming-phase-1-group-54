80aeef41050ca77e3b85a47a594885d27cb37517
Yasin HajiKhalili <seidyasinhajikhalili@gmail.com>
2025-04-24 20:48:29 +0330

    dorost kardan maven baraye ezafe karddan json

--- src/main/java/model/items/plants/Plant.java
+++ src/main/java/model/items/plants/Plant.java
@@ -1,19 +1,22 @@
 package model.items.plants;
 
 import model.Placeable;
+import model.enums.Season;
+import model.enums.Weather;
 import model.items.Item;
 
 public abstract class Plant extends Item implements Placeable {
 
-    private String name;
-    private Seed source;
+    protected String name;
+    protected Seed source;
 
-    private int[] stages;
-    private int totalHarvestTime;
-    private boolean oneTime;
-    private int regrowthTime;
-    private int baseSellPrice;
-    private boolean isEdible;
-    private int energy;
-    private boolean canBecomeGiant;
+    protected int[] stages;
+    protected int totalHarvestTime;
+    protected boolean oneTime;
+    protected int regrowthTime;
+    protected int baseSellPrice;
+    protected boolean isEdible;
+    protected int energy;
+    protected Season[] seasons;
+    protected boolean canBecomeGiant;
 }
--- src/main/java/model/items/plants/Tree.java
+++ src/main/java/model/items/plants/Tree.java
@@ -1,5 +1,17 @@
 package model.items.plants;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.Map;
+
 public class Tree extends Plant {
+    static Map<String,Tree> treeMap;
+    static {
+        ObjectMapper mapper = new ObjectMapper();
+        treeMap = mapper.convertValue(new File("trees.json"),new TypeReference<Map<String,Tree>>(){});
+    }
+
 
 }
