6aac4eed7325c382f8a4534e91eb04a941f1a93f
goodguy-not <e.razavi85@gmail.com>
2025-05-04 12:57:46 -0700

    BlackSmithShop created

---
+++ src/main/java/model/Shops/BlackSmithShop.java
@@ -0,0 +1,49 @@
+package model.Shops;
+
+import model.alive.Human;
+import model.items.ItemsInShops;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BlackSmithShop extends Shop {
+    private static ArrayList<upgradesToolsBlacsmithShop> upgradeTools;
+    private static ArrayList<ItemsInShops> Stock;
+    public BlackSmithShop(Human owner) {
+        super(owner,9,16);
+        upgradeTools = new ArrayList<>();
+        Stock = new ArrayList<>();
+    }
+
+    public ArrayList<upgradesToolsBlacsmithShop> getUpgradeTools() {
+        return upgradeTools;
+    }
+
+    public void setUpgradeTools(ArrayList<upgradesToolsBlacsmithShop> upgradeTools) {
+        upgradeTools = upgradeTools;
+    }
+
+    public ArrayList<ItemsInShops> getStock() {
+        return Stock;
+    }
+
+    public void setStock(ArrayList<ItemsInShops> stock) {
+        Stock = stock;
+    }
+
+    private class upgradesToolsBlacsmithShop extends ItemsInShops {
+        private static HashMap<String,Integer> ingredients;
+        public upgradesToolsBlacsmithShop(String name, int price) {
+            super(name,false,1, price,null);
+            ingredients = new HashMap<>();
+        }
+
+        public HashMap<String, Integer> getIngredients() {
+            return ingredients;
+        }
+
+        public void setIngredients(HashMap<String, Integer> ingredient) {
+            ingredients = ingredient;
+        }
+    }
+}
