fa3ea4df80974adc0669453ce97d8e135c3c1558
goodguy-not <e.razavi85@gmail.com>
2025-05-05 09:41:09 -0700

    BlackSmith class and datas created

--- src/main/java/model/Shops/BlackSmithShop.java
+++ src/main/java/model/Shops/BlackSmithShop.java
@@ -7,41 +7,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BlackSmithShop extends Shop {
-    private static ArrayList<upgradesToolsBlacsmithShop> upgradeTools;
-    private static ArrayList<ItemsInShops> Stock;
+    private ArrayList<upgradesToolsBlacsmithShop> upgradeTools;
+    private ArrayList<ItemsInShops> stock;
     public BlackSmithShop(Human owner) {
         super(owner,9,16);
         upgradeTools = new ArrayList<>();
-        Stock = new ArrayList<>();
+        stock = new ArrayList<>();
     }
-
     public ArrayList<upgradesToolsBlacsmithShop> getUpgradeTools() {
         return upgradeTools;
     }
 
     public void setUpgradeTools(ArrayList<upgradesToolsBlacsmithShop> upgradeTool) {
-        upgradeTools = upgradeTool;
+        this.upgradeTools = upgradeTool;
     }
 
     public ArrayList<ItemsInShops> getStock() {
-        return Stock;
+        return stock;
     }
 
     public void setStock(ArrayList<ItemsInShops> stock) {
-        Stock = stock;
+        this.stock = stock;
     }
 
-    public static class upgradesToolsBlacsmithShop extends ItemsInShops {
-        private static HashMap<String,Integer> ingredients;
+    public class upgradesToolsBlacsmithShop extends ItemsInShops {
+        private HashMap<String,Integer> ingredients;
         public upgradesToolsBlacsmithShop(String name, int price) {
-            super(name,false,1, price,null);
+            super(name,false,1, price,"no description");
             ingredients = new HashMap<>();
         }
-
         public HashMap<String, Integer> getIngredients() {
             return ingredients;
         }
-
         public void setIngredients(HashMap<String, Integer> ingredient) {
             ingredients = ingredient;
         }
