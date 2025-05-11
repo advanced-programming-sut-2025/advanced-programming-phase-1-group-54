c6aa14f84a65847341091d5dec37c152f6d6c970
Yasin HajiKhalili <seidyasinhajikhalili@gmail.com>
2025-05-11 16:22:17 +0330

    write animalHouse class

---
+++ src/main/java/model/AnimalHouse.java
@@ -0,0 +1,114 @@
+package model;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import model.alive.Animal;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AnimalHouse {
+
+    private static final HashMap<String, AnimalHouse> animalHouses;
+
+    static {
+        Gson gson = new Gson();
+        FileReader file = null;
+        try {
+            file = new FileReader("animalHouses.json");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Type type = new TypeToken<HashMap<String, AnimalHouse>>(){}.getType();
+        animalHouses = gson.fromJson(file,type);
+    }
+
+    private final String Name;
+    private final ArrayList<String> animals;
+    private final int numberOfAnimals;
+
+
+    public AnimalHouse(String name, ArrayList<String> animals, int numberOfAnimals) {
+        Name = name;
+        this.animals = animals;
+        this.numberOfAnimals = numberOfAnimals;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public ArrayList<String> getAnimals() {
+        return animals;
+    }
+
+    public int getNumberOfAnimals() {
+        return numberOfAnimals;
+    }
+
+
+
+    public static void writeToJson(){
+
+        HashMap<String,AnimalHouse> animalHousesType = new HashMap<String,AnimalHouse>();
+
+        AnimalHouse animalHouse;
+
+        animalHouse = new AnimalHouse("Coop",new ArrayList<>(){{
+            add("Hen");
+        }},4);
+        animalHousesType.put(animalHouse.getName(), animalHouse);
+
+        animalHouse = new AnimalHouse("Big Coop",new ArrayList<>(){{
+            add("Hen");
+            add("Duck");
+            add("Dinosaur");
+        }},8);
+        animalHousesType.put(animalHouse.getName(), animalHouse);
+
+        animalHouse = new AnimalHouse("Deluxe Coop",new ArrayList<>(){{
+            add("Hen");
+            add("Duck");
+            add("Rabbit");
+        }},12);
+        animalHousesType.put(animalHouse.getName(), animalHouse);
+
+        animalHouse = new AnimalHouse("Barn",new ArrayList<>(){{
+            add("Cow");
+        }},4);
+        animalHousesType.put(animalHouse.getName(), animalHouse);
+
+        animalHouse = new AnimalHouse("Big Barn",new ArrayList<>(){{
+            add("Cow");
+            add("Goat");
+        }},8);
+        animalHousesType.put(animalHouse.getName(), animalHouse);
+
+        animalHouse = new AnimalHouse("Deluxe Barn",new ArrayList<>(){{
+            add("Cow");
+            add("Goat");
+            add("Sheep");
+            add("Pig");
+        }},12);
+        animalHousesType.put(animalHouse.getName(), animalHouse);
+
+
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try (FileWriter file = new FileWriter("animalHouses.json")){
+            gson.toJson(animalHousesType, file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+}
