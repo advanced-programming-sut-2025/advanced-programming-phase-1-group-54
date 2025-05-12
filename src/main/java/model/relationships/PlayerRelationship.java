ec3d687c1212c0e9e4267c21ed20478dce92fc72
goodguy-not <e.razavi85@gmail.com>
2025-05-12 16:17:30 -0700

    added daily counts

--- src/main/java/model/relationships/PlayerRelationship.java
+++ src/main/java/model/relationships/PlayerRelationship.java
@@ -6,16 +6,52 @@ import model.alive.Player;
 import java.util.ArrayList;
 
 public class PlayerRelationship extends Friendship {
-    Player player1;
-    Player player2;
-    ArrayList<Talk> talkHistory;
-    ArrayList<Gift> giftHistory;
+    private Player player1;
+    private Player player2;
+    private ArrayList<Talk> talkHistory;
+    private ArrayList<Gift> giftHistory;
+    private int giftDailyCount;
+    private int hugDailyCount;
+    private int talkDailyCount;
+    private int partnerDailyCount;
+    //TODO  private ring
+
     public PlayerRelationship(Player player1, Player player2) {
         super();
         this.player1 = player1;
         this.player2 = player2;
         this.talkHistory = new ArrayList<>();
         this.giftHistory = new ArrayList<>();
+        this.giftDailyCount = 0;
+        this.hugDailyCount = 0;
+        this.talkDailyCount = 0;
+    }
+
+    public int getGiftDailyCount() {
+        return giftDailyCount;
+    }
+
+    public int getHugDailyCount() {
+        return hugDailyCount;
+    }
+
+    public int getTalkDailyCount() {
+        return talkDailyCount;
+    }
+    public int getPartnerDailyCount() {
+        return partnerDailyCount;
+    }
+    public void increasePartnerDailyCount() {
+        this.partnerDailyCount++;
+    }
+    public void increaseGiftDailyCount() {
+        giftDailyCount++;
+    }
+    public void increaseHugDailyCount() {
+        hugDailyCount++;
+    }
+    public void increaseTalkDailyCount() {
+        talkDailyCount++;
     }
 
     public Player getPlayer1() {
@@ -50,6 +86,7 @@ public class PlayerRelationship extends Friendship {
         this.giftHistory = giftHistory;
     }
 
+    //TODO public void setring ()
     public static ArrayList<PlayerRelationship> getPlayerRelationships(Player player) {
         ArrayList<PlayerRelationship> playerRelationships = new ArrayList<>();
         for (PlayerRelationship playerRelationship : App.getCurrentGame().getPlayerRelationships()){
@@ -58,5 +95,33 @@ public class PlayerRelationship extends Friendship {
             }
         }
         return playerRelationships;
+    }
+    public void reset(){
+        this.giftDailyCount = 0;
+        this.hugDailyCount = 0;
+        this.talkDailyCount = 0;
+    }
+    @Override
+    public void increasXp(int xp) {
+        this.xp += xp;
+        if (this.xp > (this.level + 1) * 100 && this.level < 2){
+            this.xp = 0;
+            this.level++;
+        }
+        if (this.xp < 0){
+            this.xp += this.level * 100;
+            if(this.level > 0) {
+                this.level--;
+            }
+        }
+    }
+    public void increasLevel(){
+        if (this.level < 4) {
+            this.level++;
+            this.xp = 0;
+        }
+    }
+    public void resetlevel(){
+        this.level = 0;
     }
 }
