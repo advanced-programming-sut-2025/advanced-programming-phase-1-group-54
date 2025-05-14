6dc07f222bdd34d5fe8a460e3988945417c5b082
goodguy-not <e.razavi85@gmail.com>
2025-05-13 17:05:33 -0700

    commands added

--- src/main/java/model/enums/commands/CheatCode.java
+++ src/main/java/model/enums/commands/CheatCode.java
@@ -1,6 +1,10 @@
 package model.enums.commands;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum CheatCode implements Command {
+    ADD_MONEY("cheat add (?<count>\\S) dollars")
     ; // TODO
 
     private final String regex;
@@ -12,5 +16,12 @@ public enum CheatCode implements Command {
     @Override
     public String getRegex() {
         return regex;
+    }
+    public Matcher getMatcher(String input){
+        Matcher matcher = Pattern.compile(this.regex).matcher(input);
+        if (matcher.matches()) {
+            return matcher;
+        }
+        return null;
     }
 }
