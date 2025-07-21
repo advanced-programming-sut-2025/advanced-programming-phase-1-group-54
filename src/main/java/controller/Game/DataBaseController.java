e6045c82cf8e3368b3890ddffc8d71a6c81bd195
Kourosh <kourosh.kaveh84@gmail.com>
2025-07-21 19:33:36 +0330

    fix loadJsonArray.

--- src/main/java/controller/Game/DataBaseController.java
+++ src/main/java/controller/Game/DataBaseController.java
@@ -14,8 +14,7 @@ public class DataBaseController {
         2. use gson instead of JSONArray (it is safer and easier to use)
      */
     public static JSONArray loadJsonArray(String filename) {
-        String fullPath = Paths.get("").toAbsolutePath().resolve(filename).toString();
-        try (InputStream inputStream = Objects.requireNonNull(DataBaseController.class.getClassLoader().getResourceAsStream(fullPath))) {
+        try (InputStream inputStream = Objects.requireNonNull(DataBaseController.class.getClassLoader().getResourceAsStream(filename))) {
             return new JSONArray(new JSONTokener(inputStream));
         } catch (IOException e) {
             throw new RuntimeException(e);
