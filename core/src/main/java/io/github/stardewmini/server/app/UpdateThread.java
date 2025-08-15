9b29348f86e068583bd2026d90e062f023a6ab16
Kourosh <kourosh.kaveh84@gmail.com>
2025-08-15 21:43:17 +0330

    create update

--- core/src/main/java/io/github/stardewmini/server/app/UpdateThread.java
+++ core/src/main/java/io/github/stardewmini/server/app/UpdateThread.java
@@ -1,4 +1,33 @@
 package io.github.stardewmini.server.app;
 
-public class UpdateThread {
+import io.github.stardewmini.common.JSONUtils;
+import io.github.stardewmini.common.Message;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class UpdateThread extends Thread {
+    private int TICK_RATE = 50;
+
+    private AtomicBoolean end;
+
+
+    @Override
+    public void run() {
+        while (!end.get()) {
+            // TODO
+
+            try {
+                Thread.sleep(TICK_RATE);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        end();
+    }
+
+    public void end() {
+        end.set(true);
+    }
 }
