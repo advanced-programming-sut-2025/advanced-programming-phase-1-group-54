4eacf584286bbdb7b456be4c7c17aa87a2ee2d33
Yasin HajiKhalili <seidyasinhajikhalili@gmail.com>
2025-08-16 00:33:44 +0330

    khob shash

--- core/src/main/java/io/github/stardewmini/server/controllers/UpdateController.java
+++ core/src/main/java/io/github/stardewmini/server/controllers/UpdateController.java
@@ -11,7 +11,21 @@ public class UpdateController {
 
     public static Message createAdvanceTime(int number) {
         HashMap<String, Object> body = new HashMap<>();
-        body.put("advance_time", 1);
+        body.put("advance_time", number);
         return new Message(body, Message.Type.update);
     }
+
+    public static Message createAdvanceDate(int number) {
+        HashMap<String, Object> body = new HashMap<>();
+        body.put("advance_date", number);
+        return new Message(body, Message.Type.update);
+    }
+
+    public static Message createEnergy(String requester,int number) {
+        HashMap<String, Object> body = new HashMap<>();
+        body.put("energy", number);
+        body.put("requester", requester);
+        return new Message(body, Message.Type.update);
+    }
+
 }
